0e56705b049a31221aa98ab529c96edf858eeb2f
danyel-dev <dannykill4221x@gmail.com>
2024-06-25 18:20:06 -0300

    fetching data from api

--- backend/task/views.py
+++ backend/task/views.py
@@ -7,4 +7,4 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
