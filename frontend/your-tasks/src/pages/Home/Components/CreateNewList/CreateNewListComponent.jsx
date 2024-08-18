be0ef62c64fea9e292e66bc41bbe832670c758c2
danyel-dev <dannykill4221x@gmail.com>
2024-08-18 17:08:27 -0300

    a

--- frontend/your-tasks/src/pages/Home/Components/CreateNewList/CreateNewListComponent.jsx
+++ frontend/your-tasks/src/pages/Home/Components/CreateNewList/CreateNewListComponent.jsx
@@ -4,13 +4,8 @@ import axios from 'axios';
 
 
 export default function CreateNewListComponent() {
-    const [showListCreationForm, setShowListCreationForm] = useState(false)
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
-
-    function handleChangeListCreationForm() {
-        setShowListCreationForm(!showListCreationForm)
-    }
 
     function handleSubmitForm(e) {
         e.preventDefault()
@@ -37,27 +32,22 @@ export default function CreateNewListComponent() {
 
     return(
         <div className={styles.createListContainer}>
-            <button className={styles.createListButton} onClick={handleChangeListCreationForm}>Nova Lista de Tarefas</button>
+            <form className={styles.ListCreationForm}>
+                <div className={styles.formField}>
+                    <label htmlFor="title" className={styles.formFieldLabel}>Titulo</label>
+                    <input type="text" value={title} onChange={handleChangeTitleField} id='title' />
+                </div>
 
-            {showListCreationForm? 
-                <form className={styles.ListCreationForm}>
-                    <div className={styles.formField}>
-                        <label htmlFor="title" className={styles.formFieldLabel}>Titulo</label>
-                        <input type="text" value={title} onChange={handleChangeTitleField} id='title' />
-                    </div>
+                <div className={styles.formField}>
+                    <label htmlFor="description" className={styles.formFieldLabel}>
+                        Descrição <small className={styles.smallOptional}>(Opcional)</small>
+                    </label>
 
-                    <div className={styles.formField}>
-                        <label htmlFor="description" className={styles.formFieldLabel}>
-                            Descrição <small className={styles.smallOptional}>(Opcional)</small>
-                        </label>
+                    <textarea name='description' cols="30" rows="5" id='description' value={description} onChange={handleChangeDescriptionField}></textarea>
+                </div>
 
-                        <textarea name='description' cols="30" rows="5" id='description' value={description} onChange={handleChangeDescriptionField}></textarea>
-                    </div>
-
-                    <button className={styles.buttonSubmitForm} onClick={handleSubmitForm}>Criar Lista</button>
-                </form>
-            : ""
-            }
+                <button className={styles.buttonSubmitForm} onClick={handleSubmitForm}>Criar Lista</button>
+            </form>
         </div>
     );
 }
--- frontend/your-tasks/src/pages/Home/Home.jsx
+++ frontend/your-tasks/src/pages/Home/Home.jsx
@@ -1,15 +1,37 @@
 import styles from './home.module.css'
 import CreateNewListComponent from './Components/CreateNewList/CreateNewListComponent';
 import ListTasks from './Components/ListTasks/ListTasks';
+import { useState } from 'react';
 
 
 export default function Home() {
+    const [openModal, setOpenModal] = useState(false)
+
+    function handleClick(e) {
+        var div = document.getElementById('click');
+    
+    // Verifica se o clique ocorreu fora da div
+        if (!div.contains(e.target)) {
+            setOpenModal(false)
+        }
+    }
+
     return(
         <div className={styles.container}>
+            {openModal?
+                <div className={styles.modal} onClick={handleClick}>
+                    <div id='click'>
+                        <CreateNewListComponent />
+                    </div>
+                </div>
+                : 
+                ""    
+            }
+
             <aside className={styles.aside}></aside>
 
             <main className={styles.main}>
-                <CreateNewListComponent />
+                <button onClick={() => {setOpenModal(true)}}>Clique aqui</button>
 
                 <ListTasks />
             </main>
